400a00c32076c81fdcf078714a7164faae36c6e3
Ray Valdez <ray_valdez@hotmail.co.uk>
2019-01-23 19:37:06 +0000

    Add User Signup form and validation

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+
+  get '/signup' do
+    erb :'users/signup'
+  end
+
+  post '/signup' do
+    if !(params.has_value?(""))
+      user = User.create(params)
+
+      erb :'/players/players'
+    else
+      redirect '/signup'
+    end
+  end
+end
