bb2b62a1fa2b01fae664e1db970e837f1a08d60a
Ray Valdez <ray_valdez@hotmail.co.uk>
2019-01-23 23:22:05 +0000

    Add navbar and log out feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,18 @@ class UsersController < ApplicationController
 
   post '/signup' do
     if !(params.has_value?(""))
+      @players = Player.all
       @user = User.create(params)
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
 
       erb :'/players/players'
     else
       redirect '/signup'
     end
+  end
+
+  get '/logout' do
+    session.destroy
+    redirect '/'
   end
 end
