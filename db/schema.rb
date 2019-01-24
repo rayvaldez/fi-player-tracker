8d046b995ed0f06f485a21e3fadcff5c45a34208
Ray Valdez <ray_valdez@hotmail.co.uk>
2019-01-24 21:00:55 +0000

    Add user_id column to players table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190123163029) do
+ActiveRecord::Schema.define(version: 20190124205557) do
 
   create_table "players", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20190123163029) do
     t.integer  "quantity"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
